fb30415de4830957e4d8f0f3b330a391d405f2ca
Massahgenesis <massyndjao@gmail.com>
2023-02-16 14:12:48 -0800

    File containing script that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * Function to print words with printf
- * return 0 if correct
- */
+ 
+* main - A program that print a line using the printf function
+* Return: 0 (success)
+
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,"\n);
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
