2c23772b36c36ff5f74da3497591dc8151986810
Massahgenesis <massyndjao@gmail.com>
2023-02-16 18:04:43 -0800

    File containing C program that prints exactly string and integers

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/** main -C program that prints line to the standard error
-* return: 1 (succes)
+/**main -C program that prints line to the standard error
+*return:1(succes)
 */
 int main(void)
 {
-fputs("and that piece of art is useful\"- Dora Korpar,2015-10-19\n",stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 return (1);
 }
