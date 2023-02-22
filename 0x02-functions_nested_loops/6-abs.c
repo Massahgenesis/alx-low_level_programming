8ec8ac57868e0317262e34877fe1f38a2822bb6c
Massahgenesis <massyndjao@gmail.com>
2023-02-22 13:54:32 -0800

    File containing the program that prints the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*_abs - Function that prints the absolute value of an integer
+*@a: An integer
+*Return:0
+*/
+
+int _abs(int a)
+{
+int x;
+x = abs(a);
+_putchar(x);
+return (0);
+}
