4ec69ff5a4972489353c5599487eae317873e708
Massahgenesis <massyndjao@gmail.com>
2023-02-22 01:00:14 -0800

    Modification and recompilation of 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
-* void -  keyword in c program specifies that the function doesn't return a value
+* void -keyword in c program specifies that the function doesn't return a value
+* print_alphabet - A c program that prints alphabets from a to z
 */
 
 void print_alphabet(void)
 {
-char alphabet[26] = "a-z";
+
+/* ASCII value of a=97 and z=122 */
+
 int i;
-i = 0;
-while (i < 26)
+i = 97;
+while (i <= 122)
 {
-_putchar(alphabet[i]);
+_putchar(i);
 i++;
 }
 _putchar(10);
