157720c507a45ffb76dcbd1be19df8f3ea5a7ee2
Massahgenesis <massyndjao@gmail.com>
2023-02-22 01:47:16 -0800

    File containing a program that prints alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - A c program that prints alphabet in lowercase
+ * print_alphabet - A c program that prints alphabet x10 in lowercase
  */
 
 void print_alphabet_x10(void)
