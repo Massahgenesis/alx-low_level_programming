8aa84ae43e45bbac1bf8382685f4263a7c87ecdc
Massahgenesis <massyndjao@gmail.com>
2023-02-23 15:35:02 -0800

    File containing program that prints lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*print_line - function that checks for uppercase character.
-*@n: is the integer for the paramaters of my function
-*Return: 0
-*/
+ * print_line - function that checks for uppercase character.
+ * @n: is the integer for the paramaters of my function
+ * Return: 0
+ */
 
 void print_line(int n)
 {
