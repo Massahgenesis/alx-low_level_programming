ceb4058d273eac8fcddf04739e7f283422d421fa
Massahgenesis <massyndjao@gmail.com>
2023-02-23 07:15:15 -0800

    File containing program that  multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+
+
+/**
+*mul - Function that multiplies two integers
+*@a:An integer
+*@b:An integer
+*Return:c
+*/
+
+int mul(int a, int b)
+{
+int c;
+c = a*b;
+return (c);
+}
