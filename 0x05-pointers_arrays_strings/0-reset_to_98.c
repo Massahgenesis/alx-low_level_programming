913d994ced29c537ea42c2db10a97bc14da9c217
Massahgenesis <massyndjao@gmail.com>
2023-02-27 12:14:33 -0800

    Program with function that updates pointer value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *reset_to_98 - Function that updates a pointer value
+ *@n: An integer
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0- void reset_to_98(int *n)
+* Function that updates the value of a pointers.
