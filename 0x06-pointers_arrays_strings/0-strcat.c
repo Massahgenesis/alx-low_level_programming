93df185ef7a88015e6dcdc4bf0feb2706ac32ac0
Massahgenesis <massyndjao@gmail.com>
2023-03-02 09:46:03 -0800

    Progran that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
-int k;
-while (i = 0; dest[i] != '\0')
+int i = 0;
+int j = 0;
+int k = 0;
+while (dest[i] != '\0')
 {
 i++;
 }
-while (j = 0; src[j] != '\0')
+while (src[j] != '\0')
 {
 j++;
 }
