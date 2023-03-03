61cb117ce9e15aa7fa4da7b281f9ef30349adf2e
Massahgenesis <massyndjao@gmail.com>
2023-03-02 17:16:44 -0800

    Program that concatenates two strings using at most n bytes from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,18 +22,18 @@ j++;
 }
 if (n < j)
 {
-for (k = 0; k > n; k++)
+for (k = 0; k < n; k++)
 {
 *(dest + k + i) = *(src + k);
 }
 }
 else
 {
-for (k = 0; k < n; k++)
+for (k = 0; k > n; k++)
 {
 *(dest + k + 1) = *(src + k);
 }
 }
-*(dest + k + i + 1) = '\0';
+*(dest + k + i) = 'n';
 return (dest);
 }
