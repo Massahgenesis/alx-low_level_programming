b735e4bad7fb3f78a433e2c24234f58e5e725051
Massahgenesis <massyndjao@gmail.com>
2023-03-15 05:28:33 -0700

    Program modified to fix error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,45 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
-
 /**
  *str_concat - Function that concatenates two strings
  *@s1: An integer
  *@s2: An integer
  *Return: s3
  */
-
 char *str_concat(char *s1, char *s2)
 {
 char *s3;
-int i;
-int j;
-int len;
-j = 0;
-len = 0;
+int i, j, c, len;
+c = 0, len = 0;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 {
 s2 = "";
 }
-for (i = 0; s1[i] || s2[i]; i++)
-;
+for (i = 0; s1[i] != '\0'; i++)
+{
 len++;
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+len++;
+}
 s3 = malloc(sizeof(char) * len);
 if (s3 == NULL)
 {
 return (NULL);
 }
-for (i = 0; s1[i]; i++)
+for (i = 0; s1[i] != '\0'; i++)
 {
-s3[j++] = s1[i];
+s3[c] = s1[i];
+c++;
 }
-for (i = 0; s2[i]; i++)
+for (i = 0; s2[i] != '\0'; i++)
 {
-s3[j++] = s2[i];
+s3[c] = s2[i];
+c++;
 }
 return (s3);
 }
-
-
