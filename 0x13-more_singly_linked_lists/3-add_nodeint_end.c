b9b20aa886c82b5300e21aa45e6d5bc5e3748a4e
Massahgenesis <massyndjao@gmail.com>
2023-04-12 15:01:55 -0700

    Modification added to fix output error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,6 +14,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *newNode, *lastNode;
 
 	newNode = (malloc(sizeof(listint_t)));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
 	newNode->n = n;
 	newNode->next = NULL;
 
