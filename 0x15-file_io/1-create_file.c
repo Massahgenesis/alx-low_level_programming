8e8cebb0fc6ea53e907a1a36288d32f605b2e723
Massahgenesis <massyndjao@gmail.com>
2023-04-20 08:24:02 -0700

    Output error fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,25 +15,24 @@ int create_file(const char *filename, char *text_content)
 {
 	int fp;
 	int len;
-
+	
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content != NULL)
+	{
 	for (len = 0; text_content[len] != '\0'; len++)
 	{
 	;
 	}
-	if (filename == NULL)
+	}
+	else if (filename == NULL)
 	{
 		return (-1);
 	}
-	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fp < 0)
 	{
 		return (-1);
 	}
 	write(fp, text_content, len);
-	if (text_content == NULL)
-	{
-		fp = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
-	}
 	close(fp);
 	return (1);
 
