44e3551e0b0651e5194c067c9bcefbb941852e8c
Massahgenesis <massyndjao@gmail.com>
2023-09-21 09:18:42 -0700

    The recursion project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,16 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- *_puts_recursion - Function that prints a string, followed by a new line
- *@s: An integer
+ * _puts_recursion - Function that prints a string.
+ * @s - A parameter of the function.
+ *
  */
+void _puts_recursion(char *s){
 
-void _puts_recursion(char *s)
-{
-if (*s == '\0')
-{
-_putchar('\n');
-return;
+	int i;
+	
+	i = 1;
+	if ()
+
 }
-_putchar(*s);
-_puts_recursion(s + 1);
-}
+
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,5 @@
+## C - Recursion  ##
+
 0- _puts_recursion
-* Function that prints a string, followed by a new line.
+
+ .Function that prints a string, followed by a new line.
