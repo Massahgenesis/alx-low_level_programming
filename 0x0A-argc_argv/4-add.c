262ae57a55070854081b7926eabd8801f1648989
Massahgenesis <massyndjao@gmail.com>
2023-10-28 00:57:30 +0100

    Betty error fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,12 +12,12 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, sum = 0;
-	
+
 	for (num1 = 1; num1 < argc; num1++)
 	{
 	for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 	{
-		if (!isdigit(argv[num1][num2]))
+	if (!isdigit(argv[num1][num2]))
 	{
 	printf("Error\n");
 	return (1);
