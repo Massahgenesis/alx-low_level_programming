efb1680aff3c0bea231ec464fd5221fdc272482e
Massahgenesis <massyndjao@gmail.com>
2023-10-30 23:32:47 +0000

    File containing program that frees a 2 dimensional grid previously created

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * alloc_grid - Function that returns a pointer to a 2 dimensional array.
+ * @width: Function's parameter.
+ * @height: Function's parameter.
+ * Return:g
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **g;
+	int w, h, i, j;
+
+	if (width <= 0 || height <= 0)
+	return (NULL);
+	g = malloc(sizeof(int *) * height);
+	if (g == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+	g[i] = malloc(sizeof(int) * width);
+	if (g[i] == NULL)
+	{
+	for (j = i; j >= 0; j--)
+	{
+	free(g[j]);
+	}
+	free(g);
+	return (NULL);
+	}
+	}
+	for (h = 0; h < height; h++)
+	{
+	for (w = 0; w < width; w++)
+	{
+	g[h][w] = 0;
+	}
+	}
+	return (g);
+}
