b2047df7dd1dcca35e8a45124c23ef4e109aea9b
Massahgenesis <massyndjao@gmail.com>
2023-10-30 21:26:50 +0000

    File containing program that returns a pointer to a newly allocated space in memory with the main dot cfile delete

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Function that returns a pointer.
+ * @str: Function's parameter.
+ * Return: s
+ */
+
+char *_strdup(char *str)
+{
+	char *c;
+	unsigned int i, k;
+
+	if (str == 0)
+	{
+	return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+	i++;
+	}
+	c = malloc(i * sizeof(char));
+	if (c == NULL)
+	{
+	return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+	c[k] = str[k];
+	}
+	return (c);
+}
