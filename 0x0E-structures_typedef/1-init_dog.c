ca88a14bbca04e54f6c404f1e94f41ab158939e3
Massahgenesis <massyndjao@gmail.com>
2023-11-06 23:40:37 +0000

    Program defining structure and members

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initialization of variable.
+ * @d: Dog initialize.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: owner of the dog.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
+
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+## C - Structures, typedef ##
+
+   *
+Define a new type struct dog.
