9fdc40e35ab24c3a1d8c221f5455bc3fdfb006fe
Massahgenesis <massyndjao@gmail.com>
2023-11-07 00:06:13 +0000

    File containing program that reates a new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,12 +14,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
  * dog_t - Typedef for struct dog
  */
-
 	typedef struct dog dog_t;
-
 	int _putchar(char);
 	void init_dog(struct dog *d, char *name, float age, char *owner);
 	void print_dog(struct dog *d);
