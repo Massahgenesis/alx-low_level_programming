d9dea5fcf4faec467c014d149bc76992c33b19a8
Massahgenesis <massyndjao@gmail.com>
2023-11-09 01:17:56 +0000

    Output error fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,15 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size > 0 && cmp && size && array)
 	{
-	return (-1);
-	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
-	if (cmp(array[i]) == 1)
+	if (cmp(array[i]))
 	{
 	return (i);
+	}
 	}
 	}
 	return (-1);
