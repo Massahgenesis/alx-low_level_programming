851f1f3e7f157f909fd59555b84e3a648c99ed19
Massahgenesis <massyndjao@gmail.com>
2023-11-09 01:06:29 +0000

    Files contaianing programs that executes a function given as a parameter on each element of an array and searches for an integer.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Function that executes a function as a parameter.
+ * @array: A pointer as an argument.
+ * @size: The size of the array..
+ * @action: A pointer to the function.
+ * Return: Void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+	action(array[i]);
+	}
+
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -i Function that searches for an integer.
+ * @array: The array
+ * @size: The number of the elements
+ * @cmp: Apointer to the function.
+ * Return: Index of the first element.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+	return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+	if (cmp(array[i]) == 1)
+	{
+	return (i);
+	}
+	}
+	return (-1);
+}
