d1a6bcd5a634243c9fb805460e302ea7f52879aa
Massahgenesis <massyndjao@gmail.com>
2023-11-22 10:17:41 +0000

    Modified the header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
-
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -15,7 +15,6 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
-
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
