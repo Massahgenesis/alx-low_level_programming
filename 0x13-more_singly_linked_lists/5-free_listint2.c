dce8ed3b0672b15193ac3db96b105bb5a8d290f3
Massahgenesis <massyndjao@gmail.com>
2023-11-22 14:18:25 +0000

    Program that frees a list with the head set to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that frees a list.
+ * @head: A pointer to the address of the head of the list.
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *freeList;
+
+	if (*head == NULL)
+	{
+		return;
+	}
+	while (*head != NULL)
+	{
+		freeList = (*head)->next;
+		free(*head);
+		*head = freeList;
+	}
+	*head = NULL;
+}
